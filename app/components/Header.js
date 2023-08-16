f989730fb2c11e987204e10a34390b7c71303785
Edison Estival Restrepo Ospina <edisonestival@gmail.com>
2023-08-15 22:04:53 -0500

    update

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,7 +2,6 @@ import { Menu } from "./Menu.js";
 import { SearchForm } from "./SearchForm.js";
 import { Title } from "./Title.js";
 
-
 export function Header() {
     const $header = document.createElement("header");
     $header.classList.add("header");
